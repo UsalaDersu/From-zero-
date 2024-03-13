093f3c53738a0636b970973b32330b6a1be81fd1
Elena.Mironova <elena.vy.mironova@gmail.com>
2024-03-13 16:13:41 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Даны два ненулевых числа. Найти их сумму, разность, произведение и частное. 
+double a = 0;
+double b = 0.8;
+double sum = a + b;
+double dif = a - b;
+double mult = a * b;
+double div = a / b;
+System.Console.WriteLine(sum);
+System.Console.WriteLine(dif);
+System.Console.WriteLine(mult);
+System.Console.WriteLine(div);
+System.Console.WriteLine($"Сумма: {sum}, Разность: {dif}, Произведение: {mult}, Частное: {div}");
